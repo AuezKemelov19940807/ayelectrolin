c378934bba06c00d024d89c0238fd5122e711351
Auez <kemelove@bk.ru>
2025-10-01 16:38:47 +0600

    change size equipment item

--- app/components/EquipmentItem.vue
+++ app/components/EquipmentItem.vue
@@ -7,9 +7,9 @@ const props = defineProps<{
 </script>
 
 <template>
-    <div class="flex flex-col h-full w-full ">
+    <div class="flex flex-col items-center justify-center h-full w-full ">
         <div
-            class="max-w-[460px]  max-h-[460px] w-full aspect-square  rounded-full overflow-hidden mb-4 md:mb-8 relative bg-position-center bg-no-repeat bg-cover">
+            class="max-w-[226px] sm:max-w-[460px] max-h-[226px] sm:max-h-[460px] w-full aspect-square  rounded-full overflow-hidden mb-4 md:mb-8 relative bg-position-center bg-no-repeat bg-cover">
             <img class="w-full h-full  object-cover" :src="image" alt="Equipment Item Image">
         </div>
         <p class="text-center text-lg lg:px-10 2xl:px-0  xl:text-2xl font-semibold mt-auto">
